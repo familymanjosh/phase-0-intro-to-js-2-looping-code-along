fc9dfd82ca2d7a251301c1d509fa08829178d938
+ <familymanjosh@gmail.com>
2023-02-26 15:21:11 -0800

    Pair programming

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names,event){
+    const messages = [];
+    for (let i=0; i<names.length; i++){
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message)
+    }
+    return messages
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"],"suprise"));
+
+let num =10
+function countDown(num){
+    while (num>=0){
+        console.log(num--)
+    }
+}
